b264dcd172cc7df3cae42253a82d19de5a97fb5a
Nenna Banda <nennabanda@gmail.com>
2022-04-27 15:18:16 +0200

    important-edits

---
+++ scripts/footer.js
@@ -0,0 +1,14 @@
+let appFooter = `
+
+<footer id = 'main-footer' class = 'py-2'>
+<div class="container footer-container">
+   
+    <p>
+      Copyright Nenna's News & Weather &copy; 2022, All Rights reserved. View The Code Here <a href="https://github.com/Michonenna/NNW" target="_blank"><i class="fab fa-github"></i></a>
+    </p>
+  </div>
+</div>
+</footer>
+
+`;
+document.getElementById("appFooter").innerHTML= appFooter;
---
+++ scripts/navbar.js
@@ -0,0 +1,19 @@
+let appNavbar = `
+  <nav class="navbar">
+<div class="brand"> 
+  
+    <div class="logo">NNW </div>
+    <a href="Index.html">
+
+      <div class="brand-title">Nenna's News & Weather</div>
+    </a>
+</div>
+<div class="nav-links">
+    <ul>
+        <li><a href="Index.html">Home</a></li>
+        <li><a href="Weather.html">Weather</a></li>
+    </ul>
+</div>
+</nav>
+`;
+document.getElementById("app-navbar").innerHTML = appNavbar;
